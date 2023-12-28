180d75a624bca00027f97427b012d230eb3bbc58
YokoMolina <geocondamolina3e@hotmail.com>
2023-12-28 18:35:48 -0500

    script

---
+++ Econometria_script.R
@@ -0,0 +1,13 @@
+#leamos la base de datos
+
+library(readxl)
+
+#ruta_completa <- "C:\\Users\\geoco\\OneDrive - Escuela Politécnica Nacional\\YOKO!!\\POLI\\2023-B\\Econometria\\Proyecto-Econometr-a"
+
+
+
+
+#data <- read.csv(ruta_completa, sep = ";")
+
+data <- read.csv("Video Games Sales.csv",sep = ";")
+
