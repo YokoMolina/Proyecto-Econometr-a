501a47aef397b6abce0dfeab50a23dd311a155ea
YokoMolina <geocondamolina3e@hotmail.com>
2023-12-28 19:35:22 -0500

    filtrados finales
    4  categorias

--- Econometria_script.R
+++ Econometria_script.R
@@ -11,7 +11,12 @@ data <- read.csv("Video Games Sales.csv",sep = ",")
 #filtremos los datos NA y eligamos 4 tipos de consolas 
 tipos_consola <- c("PS3", "PS4", "X360","Wii")
 
+tipos_genero <- c("Shooter", "Sports", "Action","Sports")
+
+
+
 data_new <- data |>
-  filter(Platform %in% tipos_consola)
+  filter(Platform %in% tipos_consola & Genre %in% tipos_genero)
+ 
 
 View(data_new)
